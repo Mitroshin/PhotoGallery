c0a2fc881695aa2b18ef71d83bfd3c0e49b151f0
Mitroshin <mitroshin.develop@gmail.com>
2017-04-14 20:09:53 +0300

    Реализована базовая архитектура для приложения, использующего фрагменты.

--- app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryActivity.java
+++ app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryActivity.java
@@ -1,13 +1,11 @@
 package com.developgmail.mitroshin.photogallery;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
+import android.support.v4.app.Fragment;
 
-public class PhotoGalleryActivity extends AppCompatActivity {
+public class PhotoGalleryActivity extends SingleFragmentActivity {
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_photo_gallery);
+    protected Fragment createFragment() {
+        return PhotoGalleryFragment.newInstance();
     }
 }
---
+++ app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryFragment.java
@@ -0,0 +1,10 @@
+package com.developgmail.mitroshin.photogallery;
+
+import android.support.v4.app.Fragment;
+
+public class PhotoGalleryFragment extends Fragment{
+
+    public static PhotoGalleryFragment newInstance() {
+        return new PhotoGalleryFragment();
+    }
+}
