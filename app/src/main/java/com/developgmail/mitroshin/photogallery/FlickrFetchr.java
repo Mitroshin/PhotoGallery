db1cb3247343a772b7e626811f456dabb674ab9b
Mitroshin <mitroshin.develop@gmail.com>
2017-04-14 20:53:58 +0300

    Добавлен класс и некоторые методы для работы с сетевым подключением.

---
+++ app/src/main/java/com/developgmail/mitroshin/photogallery/FlickrFetchr.java
@@ -0,0 +1,38 @@
+package com.developgmail.mitroshin.photogallery;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+public class FlickrFetchr {
+    public byte[] getUrlBytes(String urlSpec) throws IOException {
+        URL url = new URL(urlSpec);
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
+        try {
+            ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+            InputStream inputStream = connection.getInputStream();
+
+            if (connection.getResponseCode() != HttpURLConnection.HTTP_OK) {
+                throw new IOException(connection.getResponseMessage() + ": with " + urlSpec);
+            }
+
+            int bytesRead = 0;
+            byte[] buffer = new byte[1024];
+
+            while ((bytesRead = inputStream.read(buffer)) > 0) {
+                outputStream.write(buffer, 0, bytesRead);
+            }
+            outputStream.close();
+            return outputStream.toByteArray();
+        } finally {
+            connection.disconnect();
+        }
+    }
+
+    public String getUrlString(String urlSpec) throws IOException {
+        return new String(getUrlBytes(urlSpec));
+    }
+}
