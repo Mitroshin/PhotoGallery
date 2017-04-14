5c42c67f30498224056b20c482e6f73cc9d3942a
Mitroshin <mitroshin.develop@gmail.com>
2017-04-14 20:33:10 +0300

    Добавлен список на главныый экран приложения.

--- app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryFragment.java
@@ -1,10 +1,36 @@
 package com.developgmail.mitroshin.photogallery;
 
+import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.GridLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 
-public class PhotoGalleryFragment extends Fragment{
+public class PhotoGalleryFragment extends Fragment {
+
+    private RecyclerView mPhotoRecyclerView;
 
     public static PhotoGalleryFragment newInstance() {
         return new PhotoGalleryFragment();
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_photo_gallery, container, false);
+
+        mPhotoRecyclerView = (RecyclerView) view.findViewById(R.id.fragment_photo_gallery_recycler_view);
+        mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
+
+        return view;
     }
 }
