f4b09b0d252d6d0bdd795bf28bdbb927f1d74ad7
Mitroshin <mitroshin.develop@gmail.com>
2017-04-15 09:41:07 +0300

    Добавлен класс для чтения данных из сети.

--- app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryFragment.java
+++ app/src/main/java/com/developgmail/mitroshin/photogallery/PhotoGalleryFragment.java
@@ -1,15 +1,21 @@
 package com.developgmail.mitroshin.photogallery;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import java.io.IOException;
+
 public class PhotoGalleryFragment extends Fragment {
+
+    public static final String TAG = "PhotoGalleryFragment";
 
     private RecyclerView mPhotoRecyclerView;
 
@@ -21,6 +27,7 @@ public class PhotoGalleryFragment extends Fragment {
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setRetainInstance(true);
+        new FetchItemsTask().execute();
     }
 
     @Nullable
@@ -32,5 +39,18 @@ public class PhotoGalleryFragment extends Fragment {
         mPhotoRecyclerView.setLayoutManager(new GridLayoutManager(getActivity(), 3));
 
         return view;
+    }
+
+    private class FetchItemsTask extends AsyncTask<Void, Void, Void> {
+        @Override
+        protected Void doInBackground(Void... params) {
+            try {
+                String result = new FlickrFetchr().getUrlString("https://www.bignerdranch.com");
+                Log.i(TAG, "Fetched content of URL: " + result);
+            } catch (IOException e) {
+                Log.e(TAG, "Failed to fetch URL: ", e);
+            }
+            return null;
+        }
     }
 }
