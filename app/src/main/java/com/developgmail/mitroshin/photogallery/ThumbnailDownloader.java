c7eb7f996bf4e17fc96d63f0c7e8f5434abd1fad
Mitroshin <mitroshin.develop@gmail.com>
2017-04-27 18:56:28 +0300

    Добавлен класс для потоковой загрузки изображений.

---
+++ app/src/main/java/com/developgmail/mitroshin/photogallery/ThumbnailDownloader.java
@@ -0,0 +1,16 @@
+package com.developgmail.mitroshin.photogallery;
+
+import android.os.HandlerThread;
+import android.util.Log;
+
+public class ThumbnailDownloader<T> extends HandlerThread {
+    private static final String TAG = "ThumbnailDownloader";
+
+    public ThumbnailDownloader() {
+        super(TAG);
+    }
+
+    public void queueThumbnail(T target, String url) {
+        Log.i(TAG, "Got a URL " + url);
+    }
+}
